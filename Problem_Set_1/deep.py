eed515e02ff94ae5f7efa1f95db0dad365181524
Semih Tor <82044766+semihtor@users.noreply.github.com>
2023-12-13 19:38:18 +0300

    Create deep.py

---
+++ Problem_Set_1/deep.py
@@ -0,0 +1,7 @@
+userAnswer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+
+match userAnswer:
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")
