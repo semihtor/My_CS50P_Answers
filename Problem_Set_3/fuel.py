55c723d08b4cd1422ee7f53cae037ce8769af9d6
Semih Tor <82044766+semihtor@users.noreply.github.com>
2023-12-13 20:23:45 +0300

    Update fuel.py

--- Problem_Set_3/fuel.py
+++ Problem_Set_3/fuel.py
@@ -1,1 +1,30 @@
+import math
 
+while True:
+    fraction = input("Fraction: ")
+
+    try:
+        dividend, divider = fraction.split("/") # dividend=bolunen divider=bolen
+
+        new_dividend = int(dividend)
+
+        new_divider = int(divider)
+
+        fuel = new_dividend / new_divider
+
+        if fuel <= 1: # dividend and divider are both int but dividend is greater than divider
+
+            break
+
+    except (ValueError, ZeroDivisionError):
+
+        continue
+
+percentage = int(round(fuel * 100))
+
+if percentage <= 1:
+    print("E")
+elif percentage > 98:
+    print("F")
+else:
+    print(f"{math.ceil(percentage)}%")
