d2e54f18f0b2aa63b9fa974818c62a25cecb62b7
Semih Tor <82044766+semihtor@users.noreply.github.com>
2023-12-13 19:41:21 +0300

    Create plates.py

---
+++ Problem_Set_2/plates.py
@@ -0,0 +1,21 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if 2 <= len(s) <= 6 and s.isalnum() and s[0].isalpha() and s[1].isalpha():
+        for character in s:
+            if character.isdigit():
+                place = s.index(character)
+                if character != "0" and s[place:].isdigit():
+                    return True
+                else:
+                    return False
+        return True
+
+
+main()
