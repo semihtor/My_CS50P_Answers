83446d48f4511bcc6e1e9e671aeb73756aabd460
Semih Tor <82044766+semihtor@users.noreply.github.com>
2023-12-17 20:22:44 +0300

    Create test_bank.py

---
+++ Problem_Set_5/test_bank.py
@@ -0,0 +1,21 @@
+from bank import value
+
+def test_return_100():
+    assert value("What's happening?") == "$100"
+    assert value("asdfghjkl") == "$100"
+
+def test_return_twenty():
+    assert value("Hi") == "$20"
+    assert value("How you doing?") == "$20"
+    assert value("Hey") == "$20"
+    assert value("Howdy") == "$20"
+    assert value("How are you?") == "$20"
+
+
+def test_return_zero():
+    assert value("HELLO") == "$0"
+    assert value("hello") == "$0"
+    assert value("HelLo") == "$0"
+    assert value("HELLO Newman") == "$0"
+    assert value("hello Jerry") == "$0"
+    assert value("Hello Kramer") == "$0"
