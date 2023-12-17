274fd1326fe6ca3126cbc84bd6e3481e3a85c5b7
Semih Tor <82044766+semihtor@users.noreply.github.com>
2023-12-17 21:39:22 +0300

    Update test_fuel.py

--- Problem_Set_5/test_fuel.py
+++ Problem_Set_5/test_fuel.py
@@ -1,1 +1,28 @@
+import pytest
+from fuel import convert, gauge
 
+def test_correct_conversion():
+    assert convert("3/4") == 75
+    assert convert("1/4") == 25
+    assert convert("1/3") == 33
+    assert convert("2/3") == 67
+
+def test_correct_gauge():
+    assert gauge(75) == "75%"
+    assert gauge(25) == "25%"
+    assert gauge(33) == "33%"
+    assert gauge(67) == "67%"
+    assert gauge(1) == "E"
+    assert gauge(99) == "F"
+
+def test_non_integer_input():
+    with pytest.raises(ValueError):
+        convert("A/B")
+    with pytest.raises(ValueError):
+        convert("A/1")
+    with pytest.raises(ValueError):
+        convert("1/A")
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
