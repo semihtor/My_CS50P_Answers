54ae516bc5bf2092fb0739ff39f2cf6d59267565
Semih Tor <82044766+semihtor@users.noreply.github.com>
2024-01-08 21:00:46 +0300

    Update test_seasons.py

--- Problem_Set_8/test_seasons.py
+++ Problem_Set_8/test_seasons.py
@@ -1,1 +1,11 @@
+from seasons import check_date_of_birth
 
+def main():
+    test_check_date_of_birth()
+
+def test_check_date_of_birth():
+    assert check_date_of_birth("111-11-11") == None
+    assert check_date_of_birth("1111-111-11") == None
+    assert check_date_of_birth("1111-11-111") == None
+    assert check_date_of_birth("November 11th, 1111") == None
+    assert check_date_of_birth("1985-05-03") == ('1985', '05', '03')
