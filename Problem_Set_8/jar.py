3dd8bd5d0b896f80cf08576a3ca07ae4452a73b9
Semih Tor <82044766+semihtor@users.noreply.github.com>
2024-01-08 21:58:32 +0300

    Update jar.py

--- Problem_Set_8/jar.py
+++ Problem_Set_8/jar.py
@@ -1,1 +1,31 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity < 0:
+            raise ValueError("Invalid capacity")
+        self._capacity = capacity
+        self._size = 0
 
+    def __str__(self):
+        return self.size * "🍪"
+
+    def deposit(self, n):
+        if self.capacity < n:
+            raise ValueError("Capacity exceeded")
+        elif self.capacity < self.size + n:
+            raise ValueError("Too many cookies")
+        else:
+            self._size += n
+
+    def withdraw(self, n):
+        if self.size < n:
+            raise ValueError("Not enough cookies")
+        else:
+            self._size -= n
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
